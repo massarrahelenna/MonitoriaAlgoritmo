fec0ea0eb6638443c1bc2e34aa5dd8176ac0ab72
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:26:02 -0300

    Update 48-main.c

--- Switch-case/48-main.c
+++ Switch-case/48-main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int temperatura;
+    scanf("%d", &temperatura);
+
+    switch (temperatura) {
+        case 1:
+            printf("Frio\n");
+            break;
+        case 2:
+            printf("Agradável\n");
+            break;
+        case 3:
+            printf("Quente\n");
+            break;
+        default:
+            printf("Temperatura inválida\n");
+    }
+    return 0;
+}
 
