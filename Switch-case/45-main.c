4fe21e7c297006950f6c46b8447b39a2716c68c2
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:25:23 -0300

    Update 45-main.c

--- Switch-case/45-main.c
+++ Switch-case/45-main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int forma;
+    float area, comprimento, largura, raio;
+    scanf("%d", &forma);
+
+    switch (forma) {
+        case 1:
+            scanf("%f", &comprimento);
+            area = comprimento * comprimento;
+            printf("Área do quadrado: %.2f\n", area);
+            break;
+        case 2:
+            scanf("%f %f", &comprimento, &largura);
+            area = comprimento * largura;
+            printf("Área do retângulo: %.2f\n", area);
+            break;
+        case 3:
+            scanf("%f", &raio);
+            area = M_PI * raio * raio;
+            printf("Área do círculo: %.2f\n", area);
+            break;
+        default:
+            printf("Forma inválida\n");
+    }
+    return 0;
+}
 
