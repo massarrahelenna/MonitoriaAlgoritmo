7de4f95ced82dacc45a863688046d43d5c104096
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:25:45 -0300

    Update 47-main.c

--- Switch-case/47-main.c
+++ Switch-case/47-main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int opcao;
+    do {
+        printf("Menu:\n");
+        printf("1: Cadastrar\n");
+        printf("2: Consultar\n");
+        printf("3: Sair\n");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                printf("Opção Cadastrar selecionada\n");
+                break;
+            case 2:
+                printf("Opção Consultar selecionada\n");
+                break;
+            case 3:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida\n");
+        }
+    } while (opcao != 3);
+    return 0;
+}
