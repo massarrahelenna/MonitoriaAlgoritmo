9a32b0c853c8d6c8f8b3848d5b6262bf76fa634d
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:24:43 -0300

    Update 43-main.c

--- Switch-case/43-main.c
+++ Switch-case/43-main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    float nota;
+    scanf("%f", &nota);
+
+    switch ((int)nota) {
+        case 10:
+        case 9:
+            printf("A\n");
+            break;
+        case 8:
+            printf("B\n");
+            break;
+        case 7:
+            printf("C\n");
+            break;
+        case 6:
+            printf("D\n");
+            break;
+        default:
+            if (nota < 6)
+                printf("F\n");
+            else
+                printf("Nota inválida\n");
+    }
+    return 0;
+}
 
