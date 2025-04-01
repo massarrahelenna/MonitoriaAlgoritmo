6c4a114938208b2ed2e896cc8aa4884df17d549e
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:25:13 -0300

    Update 44-main.c

--- Switch-case/44-main.c
+++ Switch-case/44-main.c
@@ -7,13 +7,13 @@ int main() {
 
     switch (moeda) {
         case 1:
-            printf("Valor em Dólar: %.2f\n", valor * 5.25);  // Exemplo de cotação
+            printf("Valor em Dólar: %.2f\n", valor * 5.25);  
             break;
         case 2:
-            printf("Valor em Euro: %.2f\n", valor * 5.80);  // Exemplo de cotação
+            printf("Valor em Euro: %.2f\n", valor * 5.80);  
             break;
         case 3:
-            printf("Valor em Libra: %.2f\n", valor * 6.70);  // Exemplo de cotação
+            printf("Valor em Libra: %.2f\n", valor * 6.70);  
             break;
         default:
             printf("Moeda inválida\n");
