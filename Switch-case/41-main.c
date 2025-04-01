5b3f95b6790e2a45409c9374903bb1132efb940b
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:24:22 -0300

    Update 41-main.c

--- Switch-case/41-main.c
+++ Switch-case/41-main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() {
+    int num1, num2;
+    char op;
+    scanf("%d %d %c", &num1, &num2, &op);
+    
+    switch (op) {
+        case '+':
+            printf("%d\n", num1 + num2);
+            break;
+        case '-':
+            printf("%d\n", num1 - num2);
+            break;
+        case '*':
+            printf("%d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0)
+                printf("%.2f\n", (float)num1 / num2);
+            else
+                printf("Erro: Divisão por zero\n");
+            break;
+        default:
+            printf("Operação inválida\n");
+    }
+    return 0;
+}
