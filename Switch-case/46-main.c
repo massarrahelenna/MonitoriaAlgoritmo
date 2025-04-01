373d6b341176806f04825452ae3225373275576a
massarrahelenna <helennamassarrapaes@gmail.com>
2025-04-01 16:25:34 -0300

    Update 46-main.c

--- Switch-case/46-main.c
+++ Switch-case/46-main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int idade;
+    scanf("%d", &idade);
+
+    switch (idade / 10) {
+        case 0:
+        case 1:
+            printf("Criança\n");
+            break;
+        case 2:
+        case 3:
+            printf("Adolescente\n");
+            break;
+        case 4:
+        case 5:
+        case 6:
+            printf("Adulto\n");
+            break;
+        default:
+            printf("Idoso\n");
+    }
+    return 0;
+}
